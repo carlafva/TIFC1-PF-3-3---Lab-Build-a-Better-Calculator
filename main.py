115fbcfc60557fa75ba46e8ccc8397f2d99d4204
Carlafva <carlavelardeaz@gmail.com>
2026-09-12 11:47:56 -0500

    Complete calculator functions

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(numbers):
+    return sum(numbers)
 
-if __name__=="__main__":
-  main()
+
+def multiplymultiplenumbers(numbers):
+    resultado = 1
+
+    for number in numbers:
+        resultado = resultado * number
+
+    return resultado
+
+
+def isiteven(num):
+    return num % 2 == 0
+
+
+def isitaninteger(num):
+    return type(num) == int
+
+
+def main():
+    print(addmultiplenumbers([5, 10, 15]))
+    print(multiplymultiplenumbers([2, 3, 4]))
+    print(isiteven(4))
+    print(isiteven(7))
+    print(isitaninteger(5))
+    print(isitaninteger(5.5))
+    print("Hello learners!")
+
+
+if __name__ == "__main__":
+    main()
